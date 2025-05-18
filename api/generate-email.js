5125b8eb1adc3a29b4595b09960c11db96394379
priyansh-bot1 <manuelsonyasis45@gmail.com>
2025-05-18 21:23:27 +0800

    Update generate-email.js

--- api/generate-email.js
+++ api/generate-email.js
@@ -1,21 +1,25 @@
-const { TempMail } = require('1secmail-api');
+<Project Sdk="Microsoft.NET.Sdk">
 
-function generateRandomId() {
-    var length = 6;
-    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var randomId = '';
-    for (var i = 0; i < length; i++) {
-        randomId += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return randomId;
-}
+    <PropertyGroup>
+        <TargetFramework>net8.0</TargetFramework>
+        <Nullable>disable</Nullable>
+        <IsPackable>false</IsPackable>
+        <RootNamespace>SmorcIRL.TempMail.Tests</RootNamespace>
+        <AssemblyName>SmorcIRL.TempMail.Tests</AssemblyName>
+    </PropertyGroup>
 
-export default async (req, res) => {
-    try {
-        const mail = new TempMail(generateRandomId());
-        mail.autoFetch();
-        res.json({ email: mail.address });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
+    <ItemGroup>
+      <ProjectReference Include="..\TempMail\TempMail.csproj" />
+    </ItemGroup>
+
+    <ItemGroup>
+        <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.12.0" />
+        <PackageReference Include="NUnit" Version="4.3.2" />
+        <PackageReference Include="NUnit3TestAdapter" Version="4.6.0" />
+        <PackageReference Include="coverlet.collector" Version="6.0.3">
+          <PrivateAssets>all</PrivateAssets>
+          <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+        </PackageReference>
+    </ItemGroup>
+
+</Project>
